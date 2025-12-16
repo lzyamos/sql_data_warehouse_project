47265525401da916331c8809e31b311393f72822
AmosOmondi <98084475+lzyamos@users.noreply.github.com>
2025-12-16 00:03:43 -0500

    Enhance init_database.sql with comments
    Added comments to clarify the purpose of the script and its sections.

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,7 +1,10 @@
-CREATE DATABASE DataWarehouse;
+-- This script is for creating The database and the scemas
 
+CREATE DATABASE DataWarehouse;
+ -- Creating database
 USE DataWarehouse;
 
+-- Creating schemas
 CREATE SCHEMA bronze;
 CREATE SCHEMA silver;
 CREATE SCHEMA gold;
